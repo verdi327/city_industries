0c9a3da3cb253533955fce9beebd967e5235aa0c
michael verdi <michael.v.verdi@gmail.com>
2012-06-26 22:17:37 -0400

    changed key name to city

--- lib/most_common_industry/city_industries.rb
+++ lib/most_common_industry/city_industries.rb
@@ -10,7 +10,7 @@ module CityData
     end
 
     def self.find_by_city(address)
-      url = "http://www.city-data.com/city/#{address[:name]}-#{address[:state]}.html"
+      url = "http://www.city-data.com/city/#{address[:city]}-#{address[:state]}.html"
       doc = Nokogiri::HTML(open(url))
       industries = doc.css("#hD0_0 li").map { |item| item.child.text }
       CityData::CityIndustries.new(industries)
--- lib/most_common_industry/version.rb
+++ lib/most_common_industry/version.rb
@@ -1,3 +1,3 @@
 module MostCommonIndustry
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
